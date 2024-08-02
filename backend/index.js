81a390338c0a043deaee36995036d8e59decabd4
Suryrajsinh Jadeja <suryrajsinhjadeja95@gmail.com>
2024-08-02 15:13:06 +0530

    Containerization Completed

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,6 @@ const {Pool}=require('pg')
 const app=express()
 const port=3000;
 
-const pool = new pool({
- user: 'user'
- host: 'postgres-container'
- database : 'mydatabase'
- password : 'password'
- port : 5432, 
-})
-
 app.get('/',(req,res)=>{
  res.send('Hello from Node.js and Docker !');
 });
